2c3d15a99065a0fe8cbd8b0c7eac6518ed97fb7e
simonxue <xueyao.me@gmail.com>
2019-03-21 21:28:56 +0800

    两个数之和

---
+++ src/me/xueyao/simple/TwoSum.java
@@ -0,0 +1,37 @@
+package me.xueyao.simple;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ * @author: Simon.Xue
+ * @date: 2019-03-21 20:53
+ **/
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum(nums, 22)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i= 0;i<nums.length;i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] + nums[j] == target) {
+                    System.out.println("["+i+","+j+"]");
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
